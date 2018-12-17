ce37f25e9f0a35c44a1d7dc005a335be3457bfbb
Hagai Kariti <hkariti@gmail.com>
2018-12-17 22:39:18 +0200

    add materialNav component with router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,34 @@ import { DataviewComponent } from './dataview/dataview.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AddbillComponent } from './addbill/addbill.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'view', component: DataviewComponent },
+  { path: 'addbill', component: AddbillComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DataviewComponent,
-    AddbillComponent
+    AddbillComponent,
+    MyNavComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
