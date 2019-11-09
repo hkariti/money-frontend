2c41a09d5b8c529ed42d1442cfdddb8bf5d4921f
Hagai Kariti <hkariti@gmail.com>
2019-11-10 00:42:46 +0200

    Add account management
    Add, delete and edit accounts

--- src/app/account-view/account-view.component.ts
+++ src/app/account-view/account-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AccountService } from '../account.service';
+import { FormControl } from '@angular/forms';
+import { AccountService, AccountEntry } from '../account.service';
 
 @Component({
   selector: 'app-account-view',
@@ -9,11 +10,41 @@ import { AccountService } from '../account.service';
 export class AccountViewComponent implements OnInit {
 
   data: Object[];
-  columnsToDisplay = ['name'];
+  columnsToDisplay = ['name', 'actions'];
   accounts = this.accountService.getAll();
+  newAccountName = new FormControl('');
+  editedAccountName = new FormControl('');
+  editEntry: number;
 
   constructor(private accountService: AccountService) { }
 
   ngOnInit() {
+  }
+
+  addAccount(): void {
+    this.accountService.add(this.newAccountName.value).subscribe(this.reloadAccounts.bind(this));
+  }
+
+  enableEdit(index: number, account: AccountEntry): void {
+    this.editedAccountName.setValue(account.name);
+    this.editEntry = index;
+  }
+
+  doneEdit(account: AccountEntry): void {
+    console.log(`New name: ${this.editedAccountName.value}`);
+    this.editEntry = null;
+    this.accountService.edit(account.id, this.editedAccountName.value).subscribe(this.reloadAccounts.bind(this));
+  }
+
+  cancelEdit(): void {
+    this.editEntry = null;
+  }
+
+  deleteAccount(id: number): void {
+    this.accountService.delete(id).subscribe(this.reloadAccounts.bind(this));
+  }
+
+  reloadAccounts(): void {
+    this.accounts = this.accountService.getAll();
   }
 }
--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
+export interface AccountEntry {
+  id: number;
+  name: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,5 +19,19 @@ export class AccountService {
 
   getAll(): Observable<any> {
     return this.http.get<object[]>(this.backendUrl);
+  }
+
+  add(name: string): Observable<any> {
+    return this.http.post<object[]>(this.backendUrl, { name });
+  }
+
+  delete(id: number): Observable<any> {
+    const url = `${this.backendUrl}${id}`;
+    return this.http.delete<object[]>(url);
+  }
+
+  edit(id: number, name: string): Observable<any> {
+    const url = `${this.backendUrl}${id}/`;
+    return this.http.put<object[]>(url, { id, name });
   }
 }
